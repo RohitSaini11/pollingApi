2140adbfa2fe96b29864df0e1cf467314cedcbb6
Rohit Saini <rs0118032@gmail.com>
2023-08-07 16:25:34 +0530

    optimised

--- controllers/optionsController.js
+++ controllers/optionsController.js
@@ -11,7 +11,7 @@ module.exports.create=async function(req,res){
     });
 
     // it is for adding the vote to option of the id that is given by mongodb by update query and using the string interpolition
-    const updateOpt = await Option.findByIdAndUpdate(opt._id,{"add_vote":`http://localhost:8080/api/options/${opt._id}/add_vote`})
+    const updateOpt = await Option.findByIdAndUpdate(opt._id,{"add_vote":`http://localhost:8080/api/options/${opt._id}/add_vote`});
     updateOpt.save()
 
     // now searching the question so that we can append the option in question-->option array
@@ -25,15 +25,16 @@ module.exports.create=async function(req,res){
         res.send(ques); 
     }
     else{
-        res.send('question does not exists')
+        res.send('question does not exists');
     }
 }
 
 module.exports.add_vote = async function(req,res){
     // in this votes will be added to the particular option of the question
     console.log(req.params.id);
-    // this the increment query in which the vote is incremented by one 
-    const opt=await Option.findByIdAndUpdate(req.params.id,{ $inc: { vote: 1 }})
+
+    // to increase the vote
+    const opt = await Option.findByIdAndUpdate(req.params.id,{ $inc: { vote: 1 }})
     if(opt){
         await opt.save();
         console.log(opt);
@@ -41,27 +42,32 @@ module.exports.add_vote = async function(req,res){
     }
     // handling the bad requests
     else{
-        res.send('option does not exits')
+        res.send('Invalid Option Id : no such option found!');
     }
 }
 
 module.exports.delete = async function(req,res){
     // delete the id option 
     console.log('id',req.params.id);
-    const opt=await Option.findById(req.params.id);
+
+    //fetch the option 
+    const opt = await Option.findById(req.params.id);
     if(opt){
-        const quesId=opt.question;
-        // finding the question to which the option is deleted and removing that option from its option array
-        const ques=await Question.findByIdAndUpdate(quesId,{$pull:{options:req.params.id}});
-        // now absolutely deleting that option
-        await Option.findByIdAndDelete(req.params.id)
+        // extract the question id of the option
+        const quesId = opt.question;
+        
+        // remove the option from the question
+        const ques=await Question.findByIdAndUpdate(quesId,{ $pull: {options:req.params.id} });
+
+        // delete that option
+        await Option.findByIdAndDelete(req.params.id);
 
         console.log(ques);//remove this line 
 
-        res.send('option deleted')
+        res.send('Option removed successfully!');
     }
-    // handling the bad request
+    // if the id is not correct
     else{
-        res.send('id not exists')
+        res.send('Invalid Option ID : no such option found!');
     }
 }
--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -3,49 +3,60 @@ const Option = require('../models/options');
 
 module.exports.create = async function(req,res){
 
-    console.log(req.url);
-    console.log(req.body);
     //create the question
     await Question.create(req.body)
     .then((ques) => {
+        //question created
         console.log(ques);
         res.send(ques);
     })
     .catch((err) => {
-        console.log("error in creating the question schema",err);
+        console.log("Error in creating the question schema",err);
     });
 }
 
 module.exports.showDetails = async function(req,res){
         console.log(req.params.id);// remove this later
-        const ques=await Question.findById(req.params.id).populate('options')
+
+        const ques = await Question.findById(req.params.id).populate('options');
         if(ques){
             res.send(ques);
         }
         // if the Question does not exist
         else{
-            res.send("Invalid ID: no such ID exists!");
+            res.send("Invalid Question ID: no such question Found!");
         }
 }
 
 module.exports.deleteQues = async function(req,res){
     
         // in this the question will be deleted
-        const ques= await Question.findById(req.params.id)
+        const ques = await Question.findById(req.params.id)
         .clone()
         .catch(function(err){ 
             console.log(err);
         });
         
         if(ques){
-            // delete all the option ⁉️ of the option db having the question id as the req.params.id
-            await Question.deleteOne(req.params.id).clone().catch(function(err){ console.log(err)})
-            // deleting all the option of that question
-            await Option.deleteMany({question:req.params.id}).clone().catch(function(err){ console.log(err)})
-                res.send("ques deleted");
+            //delete the question
+            await Question.deleteOne(req.params.id)
+            .clone()
+            .catch(function(err){ 
+                console.log(err);
+            });
+
+            // delete all the option of that question
+            await Option.deleteMany({question:req.params.id})
+            .clone()
+            .catch(function(err){ 
+                console.log(err);
+            });
+
+            res.send("Question Removed successfully!");
         }
+
         //  if the Question does not exist
         else{
-            res.send("Invalid ID: no such ID exists!");
+            res.send("Invalid Question ID: no such question Found!");
         }
 }
